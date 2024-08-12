56a016817743bbbcea9352ef24a47e4e48806971
imkindzz <lawsonshi12345@gmail.com>
2024-08-12 12:29:14 +1000

    Save for Lab

---
+++ Lab2StartingPoint/Week2StartingPoint/AGP/Source/AGP/PlayerCharacter.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "InputAction.h"
+#include "InputMappingContext.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputActionValue.h"
+#include "EnhancedPlayerInput.h"
+#include "PlayerCharacter.generated.h"
+
+
+
+UCLASS()
+class AGP_API APlayerCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	APlayerCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditDefaultsOnly)
+	UInputAction* MoveAction;
+
+	UPROPERTY(EditDefaultsOnly)
+	UInputMappingContext* InputMappingContext;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
+
+
